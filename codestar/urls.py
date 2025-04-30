da3e317cfe475a3e6582f434bf5decd3a3013c3a
Ieuan Priede <priedeieuan@gmail.com>
2025-04-30 15:27:34 +0200

    Adding more posts

--- codestar/urls.py
+++ codestar/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     # the app urls are loaded as the main urls
     path("", include("blog.urls"), name="blog-urls"),
+    path('summernote/', include('django_summernote.urls')),
     path("admin/", admin.site.urls),
 ]
